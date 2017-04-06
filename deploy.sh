e6d3ecc60e84c40be98c4942092f368437583bbd
Aleksandr Sidoruk <alex.sidoruk@mail.ru>
2017-04-06 17:36:27 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,22 @@
 NGB_VERSION=$(./gradlew :printVersion |  grep "Project version is " | sed 's/^.*is //')
 
 # Deploy to dockerhub
-#CORE_REL=${DOCKERHUB_REPO}:${NGB_VERSION}-release
-#CORE_LATEST=${DOCKERHUB_REPO}:latest
+CORE_REL=${DOCKERHUB_REPO}:${NGB_VERSION}-release
+CORE_LATEST=${DOCKERHUB_REPO}:latest
 
-#DEMO_REL=${CORE_REL}-demo
-#DEMO_LATEST=${CORE_LATEST}-demo
+DEMO_REL=${CORE_REL}-demo
+DEMO_LATEST=${CORE_LATEST}-demo
 
-#docker tag ngb:latest ${CORE_REL}
-#docker tag ngb:latest ${CORE_LATEST}
+docker tag ngb:latest ${CORE_REL}
+docker tag ngb:latest ${CORE_LATEST}
 
-#docker login -p ${DOCKERHUB_PASS} -u ${DOCKERHUB_LOGIN}
+docker login -p ${DOCKERHUB_PASS} -u ${DOCKERHUB_LOGIN}
 
-#echo Pushing ${CORE_REL} to Dockerhub
-#docker push ${CORE_REL}
+echo Pushing ${CORE_REL} to Dockerhub
+docker push ${CORE_REL}
 
-#echo Pushing ${CORE_LATEST} to Dockerhub
-#docker push ${CORE_LATEST}
+echo Pushing ${CORE_LATEST} to Dockerhub
+docker push ${CORE_LATEST}
 
 # Publish binaries to demo server
 DIST="dist"
