e581a34abb5ea8e999504110e0f32449068d8203
Aleksandr Sidoruk <alex.sidoruk@mail.ru>
2017-04-06 16:58:47 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,3 +18,11 @@ docker push ${CORE_REL}
 
 echo Pushing ${CORE_LATEST} to Dockerhub
 docker push ${CORE_LATEST}
+
+# Publish binaries to demo server
+mv dist/catgenome.jar dist/catgenome-${NGB_VERSION}.jar
+mv dist/catgenome.war dist/catgenome-${NGB_VERSION}.war
+mv dist/catgenome.jar dist/catgenome-${NGB_VERSION}.jar
+mv dist/ngb-docs.tgz dist/ngb-docs-${NGB_VERSION}.tar.gz
+mv dist/ngb-cli.tar dist/ngb-cli-${NGB_VERSION}.tar
+gzip dist/ngb-cli-${NGB_VERSION}.tar
